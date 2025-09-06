3c33dc05e43f99a4ca845e36c395b5b976bd578a
oaxylf.dev <rayyt2551@gmail.com>
2025-09-06 13:42:54 +0700

    Update console-log.js
    Added variable for console function.

--- src/Week 1 - Basics and Fundamentals/day-1/console-log.js
+++ src/Week 1 - Basics and Fundamentals/day-1/console-log.js
@@ -3,3 +3,8 @@ The first thing I do is just to print, and that's where my code journey begins.
 */
 
 console.log("Hello World"); // prints out "Hello World".
+
+// You can also use variables then load it in the console as well
+
+var MyText = "Hello World"); // sets the variable's value of "MyText" into "Hello World"
+console.log(MyText) // Load the value of the "MyText" variable.
